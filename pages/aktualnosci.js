c2c36fd41f7f34b72c3b51c432c25d79ddec39cd
Daniel Król <danielkrol90@gmail.com>
2023-04-07 14:36:58 +0200

    adding basic files and assets

---
+++ pages/aktualnosci.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const aktualnosci = () => {
+  return (
+    <div>aktualnosci</div>
+  )
+}
+
+export default aktualnosci
---
+++ pages/o_firmie.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const o_firmie = () => {
+  return (
+    <div>o-firmie</div>
+  )
+}
+
+export default o_firmie
