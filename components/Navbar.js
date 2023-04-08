e63528a253ac94140dd7e79c4fe9eb337e33938e
Daniel Król <danielkrol90@gmail.com>
2023-04-08 12:15:52 +0200

    working still on navbar - mobile

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,69 +1,59 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
 import Image from "next/image";
+import { useState } from "react";
 
 import { images } from "../constants";
+import NavItem from "./NavItem";
+
+const MenuList = [
+  { text: "Strona Główna", href: "/" },
+  { text: "O Firmie", href: "/o_firmie" },
+  { text: "Aktualności", href: "/aktualnosci" },
+  { text: "Oferta", href: "/oferta" },
+  { text: "Kontakt", href: "/kontakt" },
+];
 
 const Navbar = () => {
-  const router = useRouter();
-  const currentRoute = router.pathname;
+  const [navActive, setNavActive] = useState(null);
+  const [activeIdx, setActiveIdx] = useState(-1);
 
   return (
     <header className="nav__container">
       <div className="nav__brand">
         <Link href="/">
           <Image
+            className="nav__img-desktop"
             src={images.Logo2}
+            alt="logo"
+          />
+          <Image
+            className="nav__img-mobile"
+            src={images.Logo2_v}
             alt="logo"
           />
         </Link>
       </div>
-      <nav className="nav__navbar">
-        <ul>
-          <li>
-            <Link
-              href="/"
-              className={currentRoute === "/" ? "active" : ""}
+      <nav className={`nav__navbar`}>
+        <div
+          onClick={() => setNavActive(!navActive)}
+          className={`nav__menu-bar`}
+        >
+        </div>
+        <nav className={`${navActive ? "active" : ""} nav__menu-list`}>
+          {MenuList.map((menu, idx) => (
+            <li
+              onClick={() => {
+                setActiveIdx(idx);
+                setNavActive(false);
+              }}
+              key={menu.text}
             >
-              Strona Główna
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/o_firmie"
-              className={currentRoute === "/o_firmie" ? "active" : ""}
-            >
-              O Firmie
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/aktualnosci"
-              className={currentRoute === "/aktualnosci" ? "active" : ""}
-            >
-              Aktualności
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/oferta"
-              className={currentRoute === "/oferta" ? "active" : ""}
-            >
-              Oferta
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/kontakt"
-              className={currentRoute === "/kontakt" ? "active" : ""}
-            >
-            <div/>
-              Kontakt
-            </Link>
-          </li>
-        </ul>
+              <NavItem active={activeIdx === idx} {...menu} />
+            </li>
+          ))}
+        </nav>
       </nav>
     </header>
   );
-};
+}
 export default Navbar;
