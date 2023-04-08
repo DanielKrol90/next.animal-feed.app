a437b178ee7370bab78ebfc89b05b5a2fbfb41c8
Daniel Król <danielkrol90@gmail.com>
2023-04-08 17:12:03 +0200

    navbar finished more or less

--- README.md
+++ README.md
@@ -2,5 +2,5 @@ Project created in next.js framework for learning purposes.
 
 Added to project :
 
-1) Tailwind
-2) Sass
+1) Sass,
+2) React Icons,
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 import { images } from "../constants";
 import NavItem from "./NavItem";
@@ -14,46 +15,55 @@ const MenuList = [
 ];
 
 const Navbar = () => {
-  const [navActive, setNavActive] = useState(null);
   const [activeIdx, setActiveIdx] = useState(-1);
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
 
   return (
-    <header className="nav__container">
-      <div className="nav__brand">
-        <Link href="/">
-          <Image
-            className="nav__img-desktop"
-            src={images.Logo2}
-            alt="logo"
-          />
-          <Image
-            className="nav__img-mobile"
-            src={images.Logo2_v}
-            alt="logo"
-          />
-        </Link>
-      </div>
-      <nav className={`nav__navbar`}>
-        <div
-          onClick={() => setNavActive(!navActive)}
-          className={`nav__menu-bar`}
-        >
+    <header>
+      <button
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded);
+        }}
+        className={`${isNavExpanded ? "nav__menu-bar-toggle active" : ""} nav__menu-bar-toggle`}
+      >
+        <RxHamburgerMenu size={20} />
+      </button>
+      <nav
+        className={isNavExpanded ? "nav__container hidden" : "nav__container"}
+      >
+        <div className="nav__brand">
+          <Link href="/">
+            <Image
+              className="nav__img-desktop"
+              src={images.Logo2}
+              alt="logo"
+            />
+            <Image
+              className="nav__img-mobile"
+              src={images.Logo2_v}
+              alt="logo"
+            />
+          </Link>
         </div>
-        <nav className={`${navActive ? "active" : ""} nav__menu-list`}>
-          {MenuList.map((menu, idx) => (
-            <li
-              onClick={() => {
-                setActiveIdx(idx);
-                setNavActive(false);
-              }}
-              key={menu.text}
-            >
-              <NavItem active={activeIdx === idx} {...menu} />
-            </li>
-          ))}
-        </nav>
+        <div className="nav__navbar">
+          <ul className={`${isNavExpanded ? "active" : ""} nav__menu-list`}>
+            {MenuList.map((menu, idx) => (
+              <li
+                onClick={() => {
+                  setActiveIdx(idx);
+                }}
+                key={menu.text}
+              >
+                <NavItem
+                  active={activeIdx === idx}
+                  {...menu}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
       </nav>
     </header>
   );
-}
+};
 export default Navbar;
