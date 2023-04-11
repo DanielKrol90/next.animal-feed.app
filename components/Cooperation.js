03548b1e25dd4cd5855468d8063d3f29c90f80c4
Daniel Król <danielkrol90@gmail.com>
2023-04-11 22:00:05 +0200

    finish for today, cooperation more or lest done

--- components/Cooperation.js
+++ components/Cooperation.js
@@ -6,24 +6,24 @@ import { FaQuoteRight } from "react-icons/fa";
 import data from "../constants/data";
 
 function Cooperation() {
-  const [people, setPeople] = useState(data);
+  const [company, setCompany] = useState(data);
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
-    const lastIndex = people.length - 1;
+    const lastIndex = company.length - 1;
     if (index < 0) {
       setIndex(lastIndex);
     }
     if (index > lastIndex) {
       setIndex(0);
     }
-  }, [index, people]);
+  }, [index, company]);
 
   // autoslide, clearInterval = cleanup function
   useEffect(() => {
     let slider = setInterval(() => {
       setIndex(index + 1);
-    }, 3000);
+    }, 5000);
     return () => clearInterval(slider);
   }, [index]);
 
@@ -36,15 +36,15 @@ function Cooperation() {
         </h2>
       </div>
       <div className="section-center">
-        {people.map((person, personIndex) => {
-          const { id, image, name, title, quote } = person;
+        {company.map((person, personIndex) => {
+          const { id, image, name, title } = person;
           let position = "nextSlide";
           if (personIndex === index) {
             position = "activeSlide";
           }
           if (
             personIndex === index - 1 ||
-            (index === 0 && personIndex === people.length - 1)
+            (index === 0 && personIndex === company.length - 1)
           ) {
             position = "lastSlide";
           }
@@ -54,7 +54,6 @@ function Cooperation() {
               <Image src={image} alt={name} className="person-img" width={500} height={500}/>
               <h4>{name}</h4>
               <p className="title">{title}</p>
-              <p className="text">{quote}</p>
               <FaQuoteRight className="icon" />
             </article>
             </>
--- constants/data.js
+++ constants/data.js
@@ -1,12 +1,10 @@
-const people = [
+const company = [
     {
       id: 1,
       image:
         '/../public/cooperation/de_heus.png',
       name: 'DE HEUS',
       title: 'pasze dla zwięrząt hodowlanych',
-      quote:
-        'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
     },
     {
       id: 2,
@@ -14,8 +12,6 @@ const people = [
       '/../public/cooperation/logo-barbara.jpg',
       name: 'BARBARA',
       title: 'pasze dla zwięrząt hodowlanych',
-      quote:
-        'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
     },
     {
       id: 3,
@@ -23,27 +19,21 @@ const people = [
       '/../public/cooperation/mdm.jpg',
       name: 'MDM',
       title: 'mieszanki zbożowe dla gołebi',
-      quote:
-        'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
     },
     {
       id: 4,
       image:
       '/../public/cooperation/mrowca.png',
       name: 'MROWCA',
-      title: 'the boss',
-      quote:
-        'mieszanki zbożowe dla gołebi',
+      title: 'Mieszank zbożowe dla gołebi',
     },
     {
       id: 5,
       image:
       '/../public/cooperation/dolfos.png',
       name: 'DOLFOS',
-      title: 'the boss',
-      quote:
-        'Zestawy witamin i minerałów dla zwięrząt',
+      title: 'Witaminy i minerały dla zwięrząt',
     },
   ];
   
-  export default people;
+  export default company;
